3d2bbcfae82a3e0c74f3faaa482e30a620859010
Genimarcio <genimarcio.silva@gmail.com>
2021-12-15 22:32:50 -0300

    Versão 15/12/2021

---
+++ main.js
@@ -0,0 +1,11 @@
+function alterarClasse() {
+
+    document.getElementById('alterar_classe').className = 'p-3 mb-2 bg-info text-white text-center'
+        
+}
+
+function voltar() {
+
+        document.getElementById('alterar_classe').className = 'p-3 mb-2 bg-warning text-white text-center'
+    
+}
